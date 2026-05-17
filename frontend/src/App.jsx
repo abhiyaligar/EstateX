8b44c8b996feb07f4cb0e7ccffc7c1414836f8a5
Sohanbhandari <sohanvinodbhandari@gmail.com>
2026-05-17 13:16:54 +0530

    chore: Update dependencies and core app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import LiquidChrome from './components/ui/LiquidChrome';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <AppRoutes />
-    </>
+    </ThemeProvider>
   );
 }
 
